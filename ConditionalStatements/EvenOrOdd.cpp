829f98e64a4f1d9d9c32ccdc37d443d4d021b571
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2024-02-01 11:41:57 +0200

    Add files via upload

---
+++ ConditionalStatements/EvenOrOdd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    int number;
+    cin >> number;
+    if (number % 2 == 0) {
+        cout << "even" << endl;
+    }
+    else {
+        cout << "odd" << endl;
+    }
+}
