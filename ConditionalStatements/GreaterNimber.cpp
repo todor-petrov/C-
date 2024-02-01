8b7e970d59053694c6a05b82ecaa358c8b0c6f68
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2024-02-01 09:41:16 +0200

    Add files via upload

---
+++ ConditionalStatements/GreaterNimber.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if (a > b) {
+        cout << a << endl;
+    }
+    else {
+        cout << b << endl;
+    }
+}
